c9a39272bf16020e0c1f46135e9d59ccbb970fec
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-19 23:17:49 +0300

    ft: business tests

--- hood/tests.py
+++ hood/tests.py
@@ -29,3 +29,17 @@ class PostTestClass(TestCase):
         self.post.delete_post()
         posts = Post.objects.all()
         self.assertTrue(len(posts) == 0)
+class BusinessTestClass(TestCase):
+    def setUp(self):
+        self.business = Business(name='Test Business')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business, Business))
+    def test_save_method(self):
+        self.business.save()
+        businesses = Business.objects.all()
+        self.assertTrue(len(businesses) > 0)
+    def test_delete_method(self):
+        self.business.save()
+        self.business.delete_business()
+        businesses = Business.objects.all()
+        self.assertTrue(len(businesses) == 0)
