a81fc89fe8b44d2bc5dcbc04373d69cfb8186cba
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-19 23:19:54 +0300

    ft: url patterns

---
+++ neighbor/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.contrib.auth import views
+from django_registration.backends.one_step.views import RegistrationView
+urlpatterns = [
+    
+    
+]
