57b943c151b513cbf7021b52fccdc4fee8947543
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-19 23:16:24 +0300

    ft: test locatione

---
+++ hood/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(name='Test Location')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+    def test_save_method(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+    def test_delete_method(self):
+        self.location.save_location()
+        self.location.delete()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) == 0)
