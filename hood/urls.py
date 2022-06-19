35799cb2a26ed812e630268d772e1d18273e77db
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-06-19 23:08:15 +0300

    ft: url patterns

--- hood/urls.py
+++ hood/urls.py
@@ -11,6 +11,9 @@ urlpatterns=[
     path('hood/', views.hoods, name = 'hood'),
     path('join_hood/<name>', views.join_hood, name='join-hood'),
     path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
-  
+    path('<str:name>/hood',views.single_hood,name='single_hood'),
+    path('create_post',views.create_post, name= 'create_post'),
+    path('create_business',views.create_business, name= 'create_business'),
+    path('businesses',views.businesses, name= 'businesses'),
 ]
 
